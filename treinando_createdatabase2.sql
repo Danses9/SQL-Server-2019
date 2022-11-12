292b0fc0b68363bb0f0be0be3ab8ac8143e6ee15
Danna <Danna.ses9@gmail.com>
2022-11-11 22:09:06 -0300

    Treinando create database 2

---
+++ treinando_createdatabase2.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IGUITO_VENDAS_03                     /*CRIANDO DATABASE*/
+ON (NAME = 'VENDAS_IGUITO_03',                       /*NOME INTERNO*/
+FILENAME = 'C:\IGUITO3\VENDAS_IGUITO_03.MDF',        /*NOME FISICO DA BASE*/  
+SIZE = 20MB,                                         /*TAMANHO DA BASE*/ 
+MAXSIZE = 30MB,                                      /*CAPACIDADE MAXIMA*/
+FILEGROWTH = 5MB)                                    /*QUANTIDADE DE CRESCIMENTO POR MB*/ 
+LOG ON                                               /*CRIANDO ARQUIVO DO LOG*/
+(NAME = 'VENDAS_IGUITO_03.LDF',                      /*NOME INTERNO DO LOG*/
+FILENAME = 'C:\IGUITO3\VENDAS_IGUITO_03.LDF',        /*NOME FISICO DO LOG*/
+SIZE = 20MB,                                         /*TAMANHO DA BASE*/
+MAXSIZE = 30MB,										 /*CAPACIDADE MAXIMA*/
+FILEGROWTH = 5MB);                                   /*QUANTIDADE DE CRESCIMENTO POR MB*/
